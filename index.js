c8b865d20a0202d02a88df9cf1f925223f673022
Armand Arslanian <aarslanian56@gmail.com>
2022-10-13 16:38:00 -0400

    ready to push

--- index.js
+++ index.js
@@ -1,47 +1,29 @@
-<<<<<<< HEAD
-=======
-console.log('Working')
-const numberBox = document.querySelector(".numberBox")
-console.log(numberBox)
 
->>>>>>> 468d1a735e74e82994a9ceffe7b16c29544b82f7
-// Step 1: Create a random number generator between 1-100
-// Step 2: Create an input box to enter a number. Use a button to submit that number.
-// Step 3: If not winning number, give hint as to whether guess is too high or too low.
+const numberBox = document.querySelector(".numberBox")
+const button= document.querySelector(".submit")
+
 function winNum() {
     return (Math.floor(Math.random()*100)+1)
 } 
 
-//Step 2: define and connect user given guess
-const numberBox = document.querySelector('.numberBox')
-
-function game(){
-    answer() = winningNumber ()
-}
-
-<<<<<<< HEAD
-//Step 3: logic (user gets 5 guesses)
-if (winNum() === guess1()){
-    return 'Winner!';
-} else
-=======
-
-
-function answer(){
-return (Math.floor(Math.random() * 100) + 1)
-}
-
-let button= document.querySelector(".submit")
-
 button.addEventListener('click', function(){
     let guess1 = numberBox.value
     console.log(guess1)
+})
+
+function comparison(winNum(), guess1){
+let result;
+if (winNum() === guess1){
+    result = 'Winner!';
+} else if (winNum() > guess1){
+    result = 'Hint: guess higher!'
+} else {
+    result = 'Hint: guess lower!'
 }
-)
+return result;
+}
 
 
 
 
 
-//entry into numberBox === answer <---winningNumber
->>>>>>> 468d1a735e74e82994a9ceffe7b16c29544b82f7
